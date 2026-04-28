3fa0beba4991f8dd10ddeac29f8f3f4fe80bcd00
ahmed-abdelhamid <slevin.glevra@gmail.com>
2026-04-29 02:15:04 +0300

    wire sqlc-generated Postgres queries and add register command
    Adds sqlc config, users query file, and generated database package.
    login now looks the user up in the DB before persisting to config, and a
    new register command creates users via CreateUser.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/ahmed-abdelhamid/gator
 
 go 1.26.2
+
+require github.com/google/uuid v1.6.0
+
+require github.com/lib/pq v1.12.3
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/ahmed-abdelhamid/gator/internal/config"
+	"github.com/ahmed-abdelhamid/gator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -38,11 +45,38 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("login requires a username argument")
 	}
 
-	if err := s.cfg.SetUser(cmd.args[0]); err != nil {
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if err := s.cfg.SetUser(user.Name); err != nil {
 		return err
 	}
 
-	fmt.Printf("user has been set to %s\n", cmd.args[0])
+	fmt.Printf("user has been set to %s\n", user)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("register requires a username argument")
+	}
+
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	s.cfg.SetUser(user.Name)
+	fmt.Printf("new user created: %s\n", user)
+
 	return nil
 }
 
@@ -57,12 +91,22 @@ func main() {
 		log.Fatalf("read config: %v", err)
 	}
 
-	s := &state{cfg: &cfg}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("cannot open DB: %v", err)
+	}
+	dbQueries := database.New(db)
+
+	s := &state{
+		cfg: &cfg,
+		db:  dbQueries,
+	}
 
 	cmds := &commands{
 		handlers: map[string]func(*state, command) error{},
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	cmd := command{
 		name: os.Args[1],
